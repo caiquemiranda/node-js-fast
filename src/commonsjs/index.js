a63fd85f0d2270d8f889f1bc3432055121ae20de
caiquemiranda <caiquemirandas@gmail.com>
2025-02-28 19:18:33 -0300

    add: basic codes about export modules

---
+++ src/commonsjs/index.js
@@ -0,0 +1,7 @@
+const operacoes = require("./operacoes");
+
+const resultado = operacoes.somar(10, 55);
+console.log(resultado)
+
+const resultado2 = operacoes.subitrair(10, 55);
+console.log(resultado2)
---
+++ src/commonsjs/operacoes.js
@@ -0,0 +1,12 @@
+function somar (a, b){
+    return a + b;
+}
+
+function subitrair (a, b){
+    return a - b
+}
+
+module.exports = {
+    somar,
+    subitrair,
+}
